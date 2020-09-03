08e45ef41505aff5b12d0919615c0d13672b5495
Francois Polo <andasan@gmail.com>
2020-09-03 11:35:24 -0700

    introduced controllers

--- routes/routes.js
+++ routes/routes.js
@@ -1,75 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const memberController = require('../controllers/members.controller');
 const members = require('../model/Member');
 
 //Get ALL members
-router.get('/', (req,res) => {
-    res.json(members);
-});
+router.get('/', memberController.getAllMembers);
 
 //Get ONE member
-router.get('/:id', (req,res) => {
-    const found = members.some(member => member.id === +req.params.id);
-
-    if(found){
-        res.json(members.filter(member => {
-            
-            console.log('paramsId: ', typeof req.params.id);
-            console.log('memberId', typeof member.id);
-
-            return member.id === parseInt(req.params.id)
-        })) //parseInt(req.params.id) same as +req.params.id
-    }else{
-        //400 = Bad Request
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
-    }
-});
+router.get('/:id', memberController.getOneMember);
 
 //Create a member
-router.post('/post', (req, res)=>  {
-    // res.send(req.body);
-    const newMember = {
-        id: Math.random(), //uuid
-        ...req.body
-    }
-    members.push(newMember);
-    res.json(members);
-});
+router.post('/post', memberController.createMember);
 
 //Update a member
-router.put('/update/:id', (req,res) => {
-    const found = members.some(member => member.id === +req.params.id);
-
-    if(found){
-        const updatedMember = members.map(member => {
-            if(member.id === +req.params.id){
-                return {
-                    ...member,
-                    ...req.body
-                };
-            }
-            return member;
-        });
-        res.json({ msg: 'Member updated', updatedMember});
-    }else{
-        //400 =  Bad Request
-        res.status(400).json({msg: `Unable to update. Member of id ${req.params.id} does not exist.`})
-    }
-});
+router.put('/update/:id', memberController.updateMember);
 
 //Delete a member
-router.delete('/delete/:id', (req,res) => {
-    const found = members.some(member => member.id === +req.params.id);
-
-    if(found){
-        res.json({ 
-            msg: 'Member deleted successfully', 
-            member: members.filter(member => member.id !== parseInt(req.params.id))
-        }) //parseInt(req.params.id) same as +req.params.id
-    }else{
-        //400 = Bad Request
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
-    }
-});
+router.delete('/delete/:id', memberController.deleteMember);
 
 module.exports = router;
